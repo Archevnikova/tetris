56543fba17a33b9f0649e6da22283511c06bcd76
Archevnikova <ms.orlovakatya@mail.ru>
2024-06-03 18:34:13 +0300

    feat:add element

---
+++ js/functions/create.js
@@ -0,0 +1,21 @@
+export function create(shape){
+    const lines  = shape.length;
+    const cubesInLine = shape[0].length; 
+    const figure = document.querySelector(".figure");
+
+    for (let i = 0 ; i < shape.length; i++){
+        const lineElement = document.createElement("div");
+        debugger;
+        for (let j = 0 ; j < shape[i].length; j++){
+            const cube = document.createElement("span");
+            if(shape[i][j]){
+                cube.classList.add("cube");     
+            }else{
+                cube.classList.add("empty");
+            }
+            lineElement.appendChild(cube);
+        }
+        figure.appendChild(lineElement);
+        lineElement.classList.add("line");
+    }
+}
--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 import './style.css'
-import { setupCounter } from './counter.js'
+// import { setupCounter } from './counter.js'
+import { create } from './js/functions/create.js'
+import { corner, line, square, t, z } from './js/figures/figures.js';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-  </div>
-`
 
-setupCounter(document.querySelector('#counter'))
+// document.querySelector('#app').innerHTML = `
+//   <div>
+//     <div class="card">
+//       <button id="counter" type="button"></button>
+//     </div>
+//   </div>
+// `
+
+// setupCounter(document.querySelector('#counter'))
+const shapes = [corner, line, square, t, z ];
+const index = parseInt(Math.random()*5)
+create(shapes[index]);
